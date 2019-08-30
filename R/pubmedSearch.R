cc1c1efb9cd9ad0376f30ba3241f924a95e7f975
Aaron Conway <aaronconwayphd@gmail.com>
2019-08-30 11:05:55 -0400

    change pubmed search

--- R/pubmedSearch.R
+++ R/pubmedSearch.R
@@ -3,14 +3,14 @@
 #' @return Number of results from a search of pubmed database
 #' @param keyword Character string of pubmed search
 #' 
-#' @name search
-#' @rdname search
+#' @name pubmed_search
+#' @rdname pubmed_search
 #' 
 #' @examples 
 #' search("nursing[MeSH]")
 #' 
 #' 
 #' @export
-search <- function(keyword){
+pubmed_search <- function(keyword){
   RISmed::QueryCount(RISmed::EUtilsSummary(keyword)) 
 }
